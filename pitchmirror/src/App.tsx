d849cea9cbe958ca97bbfcd6789516a90f6bbdc0
SherryKhanW <muk2010@nyu.edu>
2025-10-25 15:30:15 +0400

    Initial Vite + React + TypeScript setup with Tailwind

---
+++ pitchmirror/src/App.tsx
@@ -0,0 +1,12 @@
+import PitchMirror from './components/PitchMirror'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950">
+      <PitchMirror />
+    </div>
+  )
+}
+
+export default App
+
